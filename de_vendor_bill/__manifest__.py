78ab5496f83a7fcd945724e5bad9fbf58db3d7ab
salman7638 <79008117+salman7638@users.noreply.github.com>
2021-05-26 17:53:03 +0500

    test module vendor bill

---
+++ de_vendor_bill/__manifest__.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+#################################################################################
+# Author      : Dynexcel (<https://dynexcel.com/>)
+# Copyright(c): 2015-Present dynexcel.com
+# All Rights Reserved.
+#
+#
+#
+# This program is copyright property of the author mentioned above.
+# You can`t redistribute it and/or modify it.
+#
+#
+# You should have received a copy of the License along with this program.
+#################################################################################
+{
+  "name"                 :  "Vendor Bill Approval",
+  "summary"              :  "This Module add the approval of vendor bill functionality in vendor bills",
+  "category"             :  "Accounting",
+  "version"              :  "1.1",
+  "sequence"             :  1,
+  "author"               :  "Dynexcel",
+  "license"              :  "AGPL-3",
+  "website"              :  "http://dynexcel.com",
+  "description"          :  """
+
+""",
+  "live_test_url"        :  "",
+  "depends"              :  [
+                             'account','purchase'
+                            ],
+  "data"                 :  [
+                            'security/ir.model.access.csv',
+                            'views/vendor_bill_group_view.xml',
+                            'views/vendor_bill_state_view.xml',
+                            'views/vendor_bill_view.xml'
+                            ],
+  "images"               :  ['static/description/Banner.png'],
+  "application"          :  True,
+  "installable"          :  True,
+  "auto_install"         :  False,
+  "price"                :  0,
+  "currency"             :  "EUR",
+  "images"		 :['static/description/banner.jpg'],
+}
